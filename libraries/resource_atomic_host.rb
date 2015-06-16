e539fb416ff7b1199ef58f239b5ece365c9227cb
Adam Leff <aleff@chef.io>
2015-06-16 17:25:38 -0400

    updating README with more documentation, adding license header to all files

--- libraries/resource_atomic_host.rb
+++ libraries/resource_atomic_host.rb
@@ -1,3 +1,21 @@
+#
+# Author:: Chef Partner Engineering (<partnereng@chef.io>)
+# Copyright:: Copyright (c) 2015 Chef Software, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 class Chef
   class Resource
     class AtomicHost < Chef::Resource::LWRPBase
@@ -13,7 +31,7 @@ class Chef
       attribute :ssh_keys,    kind_of: Array, default: []
       attribute :ram,         kind_of: [Integer, String], default: 2048
       attribute :cpus,        kind_of: [Integer, String], default: 2
-      attribute :role,        kind_of: [String, Symbol], default: 'master'
+      attribute :role,        kind_of: [String, Symbol], required: true
       attribute :master_ip,   kind_of: String, default: nil
       attribute :node_ips,    kind_of: Array, default: []
 
--- recipes/_bootstrap.rb
+++ recipes/_bootstrap.rb
@@ -1,3 +1,21 @@
+#
+# Author:: Chef Partner Engineering (<partnereng@chef.io>)
+# Copyright:: Copyright (c) 2015 Chef Software, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 remote_image = "#{node['atomic']['image_base_uri']}/#{node['atomic']['image_version']}"
 compressed_image_file = "#{node['atomic']['work_dir']}/#{node['atomic']['image_version']}"
 image_file = compressed_image_file.sub('qcow2.xz', 'qcow2')
